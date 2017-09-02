3d7bb1129b4a21adbf2a7eef85bfe6b9bf5e9430
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-09-02 13:32:21 -0700

    add hover

---
+++ js/master.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#discover-sjsu').hover(function(){
+        $('.discover-sjsu-dropdown').show();
+    }, function(){
+        $('.discover-sjsu-dropdown').hide();
+    });
+    $('#future-students').hover(function(){
+        $('.future-students-dropdown').show();
+    }, function(){
+        $('.future-students-dropdown').hide();
+    })
+});
